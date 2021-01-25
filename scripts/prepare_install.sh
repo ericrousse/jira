e9d51c33b523404ebc5158578563a314baf6edb7
Eric Rousse <ericrousse@gmail.com>
2021-01-25 15:16:13 -0500

    tweak

--- scripts/prepare_install.sh
+++ scripts/prepare_install.sh
@@ -87,6 +87,7 @@ function download_installer {
 function install_pacapt {
   wget -O /usr/local/bin/pacapt https://github.com/icy/pacapt/raw/ng/pacapt
   sudo chmod 755 /usr/local/bin/pacapt
+  #PATH=$PATH:/usr/local/bin
 }
 
 function install_redhat_epel_if_needed {
@@ -646,7 +647,7 @@ function remount_share {
 
 function prepare_datadisks {
   atl_log prepare_datadisks "Preparing data disks, striping, adding to fstab"
-  ./vm-disk-utils-0.1.sh -b "/datadisks" -o "noatime,nodiratime,nodev,noexec,nosuid,nofail,barrier=0" -s
+  ./vm-disk-utils-0.1.sh -b "/datadisks" -o "noatime,nodiratime,nodev,noexec,nosuid,nofail,barrier=0"
   atl_log prepare_datadisks "Creating symlink from [${ATL_JIRA_HOME}] to striped disk at [/datadisks/disk1]"
   mkdir -p $(dirname "${ATL_JIRA_HOME}")
   ln -d -s "/datadisks/disk1" "${ATL_JIRA_HOME}"
