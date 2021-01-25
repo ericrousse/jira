8c74406e914e23fff4cb96834f046fae6677cc10
Eric Rousse <ericrousse@gmail.com>
2021-01-25 15:27:29 -0500

    retrait espace blanc

--- scripts/vm-disk-utils-0.1.sh
+++ scripts/vm-disk-utils-0.1.sh
@@ -10,10 +10,10 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,9 +33,9 @@
 # Parameters :
 #  1 - b: The base directory for mount points (default: /datadisks)
 #  2 - s  Create a striped RAID0 Array (No redundancy)
-#  3 - h  Help 
+#  3 - h  Help
 #  4 - o  Mount options for mount points
-# Note : 
+# Note :
 # This script has only been tested on Ubuntu 12.04 LTS and must be root
 
 help()
@@ -113,7 +113,7 @@ is_partitioned() {
         return 1
     else
         return 0
-    fi    
+    fi
 }
 
 has_filesystem() {
@@ -281,11 +281,11 @@ create_striped_volume()
 	    PARTITIONS+=("${PARTITION}")
 	done
 
-    MDDEVICE=$(get_next_md_device)    
+    MDDEVICE=$(get_next_md_device)
 	udevadm control --stop-exec-queue
 	mdadm --create ${MDDEVICE} --level 0 -c 64 --raid-devices ${#PARTITIONS[@]} ${PARTITIONS[*]}
 	udevadm control --start-exec-queue
-	
+
 	MOUNTPOINT=$(get_next_mountpoint)
 	echo "Next mount point appears to be ${MOUNTPOINT}"
 	[ -d "${MOUNTPOINT}" ] || mkdir -p "${MOUNTPOINT}"
