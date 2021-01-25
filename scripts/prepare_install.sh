0738201ad3aaf775a80fafc9d84599b65083c6d3
Eric Rousse <ericrousse@gmail.com>
2021-01-25 15:24:27 -0500

    bleh

--- scripts/prepare_install.sh
+++ scripts/prepare_install.sh
@@ -647,7 +647,7 @@ function remount_share {
 
 function prepare_datadisks {
   atl_log prepare_datadisks "Preparing data disks, striping, adding to fstab"
-  ./vm-disk-utils-0.1.sh -b "/datadisks" -o "noatime,nodiratime,nodev,noexec,nosuid,nofail,barrier=0"
+  ./vm-disk-utils-0.1.sh -b "/datadisks" -o "noatime,nodiratime,nodev,noexec,nosuid,nofail,barrier=0" -s
   atl_log prepare_datadisks "Creating symlink from [${ATL_JIRA_HOME}] to striped disk at [/datadisks/disk1]"
   mkdir -p $(dirname "${ATL_JIRA_HOME}")
   ln -d -s "/datadisks/disk1" "${ATL_JIRA_HOME}"
